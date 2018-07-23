6fcf631dc8e13e09a199dcf1c878a183dabc137b
Zhrey <zs1466439824@sina.com>
2018-07-23 22:48:32 +0800

    新增jetty容器，eureka

--- framework-jetty/src/main/java/com/ray/cloud/framework/eureka/JettyFrameWorkServer.java
+++ framework-jetty/src/main/java/com/ray/cloud/framework/eureka/JettyFrameWorkServer.java
@@ -1,15 +1,23 @@
-package com.ray.cloud.framework.jetty;
+package com.ray.cloud.framework.eureka;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @ComponentScan(basePackages = {"com.ray.cloud.framework"})
 public class JettyFrameWorkServer extends SpringBootServletInitializer {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     @Primary
